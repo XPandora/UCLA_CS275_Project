69dd5f01a68d72d50dd0bfcd0c44549862811f07
Yadi Cao <yadicao95@ucla.edu>
2022-05-20 23:33:43 -0700

    Fish derives

---
+++ Assets/Scripts/FishPrey.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishPrey : FishBase {
+    // YADI NOTE: I am not familiar with the derive in C sharp, so may be you need to add more stuff here i.e. construction overload?
+    intention GenerateIntentioBasedOnHabit(){
+        // TODO overload
+    }
+}
